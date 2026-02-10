d973d92b2f61565cd35d1121f6cf9a8817613e41
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2026-02-10 14:30:18 -0300

    Imagem do Goku adicionada

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 //Torna o js mais rígido com seu código
 'use strict'
 
-//Criação de variável
-const botaoTrocarCor = document.getElementById('botao-trocar-cor')
-const botaoVerde = document.getElementById('botao-verde')
-const botaoVermelho = document.getElementById('botao-vermelho')
+    //Criação de variável
+    const botaoTrocarCor = document.getElementById('botao-trocar-cor')
+    const botaoVerde = document.getElementById('botao-verde')
+    const botaoVermelho = document.getElementById('botao-vermelho')
 
-//Função para trocar a cor de fundo
-function trocarCor() {
-    //Pega a cor informada pelo usuário
-    const corUsuario = document.getElementById('cor-usuario').value
+    //Função para trocar a cor de fundo
+    function trocarCor() {
+        //Pega a cor informada pelo usuário
+        const corUsuario = document.getElementById('cor-usuario').value
 
-    //Trocar cores em português
-    if (corUsuario == 'marrom') {
-        document.documentElement.style.setProperty('--color-bg', 'brown')
-    } else if (corUsuario == 'lilas' || corUsuario == 'lilás') {
-        document.documentElement.style.setProperty('--color-bg', '#A47DAB')
-    } else if (corUsuario == 'rosa-choque') {
-        document.documentElement.style.setProperty('--color-bg', 'hotpink')
-    } else if (corUsuario == 'azul') {
-        document.documentElement.style.setProperty('--color-bg', 'blue')
-    } else {
-        document.documentElement.style.setProperty('--color-bg', corUsuario)
+        //Trocar cores em português
+        if (corUsuario == 'marrom') {
+            document.documentElement.style.setProperty('--color-bg', 'brown')
+        } else if (corUsuario == 'lilas' || corUsuario == 'lilás') {
+            document.documentElement.style.setProperty('--color-bg', '#A47DAB')
+        } else if (corUsuario == 'rosa-choque') {
+            document.documentElement.style.setProperty('--color-bg', 'hotpink')
+        } else if (corUsuario == 'azul') {
+            document.documentElement.style.setProperty('--color-bg', 'blue')
+        } else {
+            document.documentElement.style.setProperty('--color-bg', corUsuario)
+        }
+
     }
 
-}
+    function trocarCorVerde() {
+        document.documentElement.style.setProperty('--color-bg', 'green')
+    }
 
-function trocarCorVerde() {
-    document.documentElement.style.setProperty('--color-bg', 'green')
-}
+    function trocarCorVermelho() {
+        document.documentElement.style.setProperty('--color-bg', 'red')
+    }
 
-function trocarCorVermelho() {
-    document.documentElement.style.setProperty('--color-bg', 'red')
-}
-
-botaoTrocarCor.addEventListener('click', trocarCor)
-botaoVerde.addEventListener('click', trocarCorVerde)
-botaoVermelho.addEventListener('click', trocarCorVermelho)
+    botaoTrocarCor.addEventListener('click', trocarCor)
+    botaoVerde.addEventListener('click', trocarCorVerde)
+    botaoVermelho.addEventListener('click', trocarCorVermelho)
