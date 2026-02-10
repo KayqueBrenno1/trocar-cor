317aea0af62822339bc07d1800f26540ab86e749
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2026-02-10 14:00:53 -0300

    Propriedades de escolha em português

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ const botaoTrocarCor = document.getElementById('botao-trocar-cor')
 function trocarCor() {
     //Pega a cor informada pelo usuário
     const corUsuario = document.getElementById('cor-usuario').value
-    document.documentElement.style.setProperty('--color-bg', corUsuario)
+
+    if (corUsuario == 'marrom') {
+        document.documentElement.style.setProperty('--color-bg', 'brown')
+    } else if (corUsuario == 'lilas' || corUsuario == 'lilás') {
+        document.documentElement.style.setProperty('--color-bg', '#A47DAB')
+    } else if (corUsuario == 'rosa-choque') {
+        document.documentElement.style.setProperty('--color-bg', 'hotpink')
+    } else if (corUsuario == 'azul') {
+        document.documentElement.style.setProperty('--color-bg', 'blue')
+    } else {
+        document.documentElement.style.setProperty('--color-bg', corUsuario)
+    }
 
 }
 
