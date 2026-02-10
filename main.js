10a434e5397f0c71b7b3e740ca3b75ddf666bb5d
KAYQUE BRENNO FERREIRA ALMEIDA <25203698@CFP127ALUNO.LOCAL>
2026-02-10 16:38:27 -0300

    Trocadilho adicionado

--- main.js
+++ main.js
@@ -5,6 +5,7 @@
     const botaoTrocarCor = document.getElementById('botao-trocar-cor')
     const botaoVerde = document.getElementById('botao-verde')
     const botaoVermelho = document.getElementById('botao-vermelho')
+    const urlDaImagem = './img/dragon-ball.jpg'
 
     //Função para trocar a cor de fundo
     function trocarCor() {
@@ -13,24 +14,34 @@
 
         //Trocar cores em português
         if (corUsuario == 'marrom') {
+            document.body.style.backgroundImage = 'none'
             document.documentElement.style.setProperty('--color-bg', 'brown')
         } else if (corUsuario == 'lilas' || corUsuario == 'lilás') {
+            document.body.style.backgroundImage = 'none'
             document.documentElement.style.setProperty('--color-bg', '#A47DAB')
         } else if (corUsuario == 'rosa-choque') {
+            document.body.style.backgroundImage = 'none'
             document.documentElement.style.setProperty('--color-bg', 'hotpink')
         } else if (corUsuario == 'azul') {
+            document.body.style.backgroundImage = 'none'
             document.documentElement.style.setProperty('--color-bg', 'blue')
+        } else if(corUsuario == 'goku') {
+            document.body.style.backgroundImage = `url('${urlDaImagem}')`
+            document.documentElement.style.setProperty('--color-bg', 'transparent')
         } else {
+            document.body.style.backgroundImage = 'none'
             document.documentElement.style.setProperty('--color-bg', corUsuario)
         }
 
     }
 
     function trocarCorVerde() {
+        document.body.style.backgroundImage = 'none'
         document.documentElement.style.setProperty('--color-bg', 'green')
     }
 
     function trocarCorVermelho() {
+        document.body.style.backgroundImage = 'none'
         document.documentElement.style.setProperty('--color-bg', 'red')
     }
 
